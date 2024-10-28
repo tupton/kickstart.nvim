665e63614c70f63b13e35d4126951e6fe9bf1e8e
Thomas Upton <thomasupton@gmail.com>
2024-10-27 23:26:57 -0500

    Reenable treesitter highlighting

--- init.lua
+++ init.lua
@@ -1030,7 +1030,7 @@ require('lazy').setup({
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
-        enable = false,
+        enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
