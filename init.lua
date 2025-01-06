dde42ff222e54779c334bd32635b1e15fbfa89a3
Thomas Upton <thomasupton@gmail.com>
2025-01-06 17:12:35 -0600

    Trial: revert to which-key over mini.clue

--- init.lua
+++ init.lua
@@ -400,14 +400,70 @@ require('lazy').setup({
   -- This is often very useful to both group configuration, as well as handle
   -- lazy loading plugins that don't need to be loaded immediately at startup.
   --
-  -- For example, the following:
+  -- For example, in the following configuration, we use:
   --  event = 'VimEnter'
   --
-  -- loads the plugin before all the UI elements are loaded. Events can be
+  -- which loads which-key before all the UI elements are loaded. Events can be
   -- normal autocommands events (`:help autocmd-events`).
   --
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
+
+  { -- Useful plugin to show you pending keybinds.
+    'folke/which-key.nvim',
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    opts = {
+      -- delay between pressing a key and opening which-key (milliseconds)
+      -- this setting is independent of vim.opt.timeoutlen
+      delay = 0,
+      icons = {
+        -- set icon mappings to true if you have a Nerd Font
+        mappings = vim.g.have_nerd_font,
+        -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+        -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
+        keys = vim.g.have_nerd_font and {} or {
+          Up = '<Up> ',
+          Down = '<Down> ',
+          Left = '<Left> ',
+          Right = '<Right> ',
+          C = '<C-…> ',
+          M = '<M-…> ',
+          D = '<D-…> ',
+          S = '<S-…> ',
+          CR = '<CR> ',
+          Esc = '<Esc> ',
+          ScrollWheelDown = '<ScrollWheelDown> ',
+          ScrollWheelUp = '<ScrollWheelUp> ',
+          NL = '<NL> ',
+          BS = '<BS> ',
+          Space = '<Space> ',
+          Tab = '<Tab> ',
+          F1 = '<F1>',
+          F2 = '<F2>',
+          F3 = '<F3>',
+          F4 = '<F4>',
+          F5 = '<F5>',
+          F6 = '<F6>',
+          F7 = '<F7>',
+          F8 = '<F8>',
+          F9 = '<F9>',
+          F10 = '<F10>',
+          F11 = '<F11>',
+          F12 = '<F12>',
+        },
+      },
+
+      -- Document existing key chains
+      spec = {
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>g', group = '[G]it' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>x', group = 'Diagnostics' },
+      },
+    },
+  }, -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
 
   -- NOTE: Plugins can specify dependencies.
   --
@@ -998,60 +1054,60 @@ require('lazy').setup({
         },
       }
 
-      local miniclue = require 'mini.clue'
-      miniclue.setup { -- cute prompts about bindings
-        triggers = {
-          { mode = 'n', keys = '<Leader>' },
-          { mode = 'x', keys = '<Leader>' },
-
-          -- Built-in completion
-          { mode = 'i', keys = '<C-x>' },
-
-          -- `g` key
-          { mode = 'n', keys = 'g' },
-          { mode = 'x', keys = 'g' },
-
-          -- Marks
-          { mode = 'n', keys = "'" },
-          { mode = 'n', keys = '`' },
-          { mode = 'x', keys = "'" },
-          { mode = 'x', keys = '`' },
-
-          -- Registers
-          { mode = 'n', keys = '"' },
-          { mode = 'x', keys = '"' },
-          { mode = 'i', keys = '<C-r>' },
-          { mode = 'c', keys = '<C-r>' },
-
-          -- Window commands
-          { mode = 'n', keys = '<C-w>' },
-
-          -- `z` key
-          { mode = 'n', keys = 'z' },
-          { mode = 'x', keys = 'z' },
-
-          -- Bracketed
-          { mode = 'n', keys = '[' },
-          { mode = 'n', keys = ']' },
-
-          -- Option toggles
-          { mode = 'n', keys = '\\' },
-        },
-        clues = {
-          miniclue.gen_clues.builtin_completion(),
-          miniclue.gen_clues.g(),
-          miniclue.gen_clues.marks(),
-          miniclue.gen_clues.registers(),
-          miniclue.gen_clues.windows(),
-          miniclue.gen_clues.z(),
-        },
-        window = {
-          delay = 100,
-          config = { width = 'auto' },
-          scroll_down = '<C-d>',
-          scroll_up = '<C-u>',
-        },
-      }
+      -- local miniclue = require 'mini.clue'
+      -- miniclue.setup { -- cute prompts about bindings
+      --   triggers = {
+      --     { mode = 'n', keys = '<Leader>' },
+      --     { mode = 'x', keys = '<Leader>' },
+      --
+      --     -- Built-in completion
+      --     { mode = 'i', keys = '<C-x>' },
+      --
+      --     -- `g` key
+      --     { mode = 'n', keys = 'g' },
+      --     { mode = 'x', keys = 'g' },
+      --
+      --     -- Marks
+      --     { mode = 'n', keys = "'" },
+      --     { mode = 'n', keys = '`' },
+      --     { mode = 'x', keys = "'" },
+      --     { mode = 'x', keys = '`' },
+      --
+      --     -- Registers
+      --     { mode = 'n', keys = '"' },
+      --     { mode = 'x', keys = '"' },
+      --     { mode = 'i', keys = '<C-r>' },
+      --     { mode = 'c', keys = '<C-r>' },
+      --
+      --     -- Window commands
+      --     { mode = 'n', keys = '<C-w>' },
+      --
+      --     -- `z` key
+      --     { mode = 'n', keys = 'z' },
+      --     { mode = 'x', keys = 'z' },
+      --
+      --     -- Bracketed
+      --     { mode = 'n', keys = '[' },
+      --     { mode = 'n', keys = ']' },
+      --
+      --     -- Option toggles
+      --     { mode = 'n', keys = '\\' },
+      --   },
+      --   clues = {
+      --     miniclue.gen_clues.builtin_completion(),
+      --     miniclue.gen_clues.g(),
+      --     miniclue.gen_clues.marks(),
+      --     miniclue.gen_clues.registers(),
+      --     miniclue.gen_clues.windows(),
+      --     miniclue.gen_clues.z(),
+      --   },
+      --   window = {
+      --     delay = 100,
+      --     config = { width = 'auto' },
+      --     scroll_down = '<C-d>',
+      --     scroll_up = '<C-u>',
+      --   },
+      -- }
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
