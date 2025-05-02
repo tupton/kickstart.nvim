29e05808ff8f75bc89a25e57af29d234335c3c3d
Thomas Upton <tom@wizehire.com>
2025-05-02 10:41:39 -0500

    nit: formatting for mini.ai setup

--- init.lua
+++ init.lua
@@ -1036,7 +1036,10 @@ require('lazy').setup({
       local spec_treesitter = require('mini.ai').gen_spec.treesitter
       require('mini.ai').setup {
         custom_textobjects = {
-          F = spec_treesitter { a = '@function.outer', i = '@function.inner' },
+          F = spec_treesitter {
+            a = '@function.outer',
+            i = '@function.inner',
+          },
           o = spec_treesitter {
             a = { '@conditional.outer', '@loop.outer' },
             i = { '@conditional.inner', '@loop.inner' },
