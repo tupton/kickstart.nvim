1b133f773eec2eef4ac02118a7e8948fd400c238
Thomas Upton <thomasupton@gmail.com>
2025-11-22 19:02:28 -0600

    Remove unused diagnostic formatters

--- init.lua
+++ init.lua
@@ -670,16 +670,8 @@ require('lazy').setup {
           virtual_text = {
             source = 'if_many',
             spacing = 2,
-            format = function(diagnostic)
-              local diagnostic_message = {
-                [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                [vim.diagnostic.severity.WARN] = diagnostic.message,
-                [vim.diagnostic.severity.INFO] = diagnostic.message,
-                [vim.diagnostic.severity.HINT] = diagnostic.message,
-              }
-              return diagnostic_message[diagnostic.severity]
-            end,
           },
+          -- virtual_lines = true,
         }
 
         -- LSP servers and clients are able to communicate to each other what features they support.
