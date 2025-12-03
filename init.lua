91bba77db480f64721eda4d257236d40d9adb220
Thomas Upton <thomasupton@gmail.com>
2025-12-03 10:01:16 -0600

    Disable colorful backgrounds in diff mode
    It's distractingly high-contrast, especially when looking at unified diffs e.g. for NES via Sidekick.

--- init.lua
+++ init.lua
@@ -1191,6 +1191,7 @@ require('lazy').setup {
         vim.o.background = 'dark'
         vim.g.nord_contrast = true
         vim.g.nord_borders = true
+        vim.g.nord_uniform_diff_background = true
         vim.cmd.colorscheme 'nord'
       end,
     },
