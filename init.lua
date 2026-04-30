31dcc6f51e4f6a81e2274ccbdaaf2cb086080307
Thomas Upton <thomasupton@gmail.com>
2026-04-30 08:44:30 -0500

    Remove unnecessary which-key config

--- init.lua
+++ init.lua
@@ -564,42 +564,7 @@ require('lazy').setup {
         -- delay between pressing a key and opening which-key (milliseconds)
         -- this setting is independent of vim.o.timeoutlen
         delay = 100,
-        icons = {
-          -- set icon mappings to true if you have a Nerd Font
-          mappings = vim.g.have_nerd_font,
-          -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-          -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-          keys = vim.g.have_nerd_font and {} or {
-            Up = '<Up> ',
-            Down = '<Down> ',
-            Left = '<Left> ',
-            Right = '<Right> ',
-            C = '<C-…> ',
-            M = '<M-…> ',
-            D = '<D-…> ',
-            S = '<S-…> ',
-            CR = '<CR> ',
-            Esc = '<Esc> ',
-            ScrollWheelDown = '<ScrollWheelDown> ',
-            ScrollWheelUp = '<ScrollWheelUp> ',
-            NL = '<NL> ',
-            BS = '<BS> ',
-            Space = '<Space> ',
-            Tab = '<Tab> ',
-            F1 = '<F1>',
-            F2 = '<F2>',
-            F3 = '<F3>',
-            F4 = '<F4>',
-            F5 = '<F5>',
-            F6 = '<F6>',
-            F7 = '<F7>',
-            F8 = '<F8>',
-            F9 = '<F9>',
-            F10 = '<F10>',
-            F11 = '<F11>',
-            F12 = '<F12>',
-          },
-        },
+        icons = { mappings = vim.g.have_nerd_font },
 
         -- Document existing key chains
         spec = {
@@ -608,8 +573,8 @@ require('lazy').setup {
           { '<leader>g', group = '[G]it' },
           { '<leader>x', group = 'Diagnostics' },
           { '<leader>a', group = 'Sidekick', mode = { 'n', 'x' } },
-          { 'gr', group = 'LSP Actions', mode = { 'n' } },
           { 'C-w', group = '[W]indow', mode = { 'n' } },
+          { 'gr', group = 'LSP Actions', mode = { 'n' } },
         },
       },
     }, -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
